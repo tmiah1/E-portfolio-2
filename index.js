07a27fc0ee7912e419f4963dc11d0e07e9baada4
ahmed <ahmedtalha.miah@gmail.com>
2023-07-19 07:22:33 +0100

    initialized git repository

--- index.js
+++ index.js
@@ -4,5 +4,13 @@
 
 function contact(event) {
     event.preventDefault();
-    console.log('it worked')
+    email.js
+    .sendform(
+        'service_1l4euvi'
+        'template_v00ff5p'
+        event.target,
+        'Kr_81zp2OH0bmi1KC'
+    ).then(() => {
+        console.log('this worked1')
+    })
 }
