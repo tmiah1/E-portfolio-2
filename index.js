c5351e7327101eda79975386c6180e194a3c325a
ahmed <ahmedtalha.miah@gmail.com>
2023-07-22 21:54:01 +0100

    initialized git repository

--- index.js
+++ index.js
@@ -4,13 +4,25 @@
 
 function contact(event) {
     event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible";
     email.js
     .sendform(
-        'service_1l4euvi'
-        'template_v00ff5p'
+        'service_1l4euvi',
+        'template_v00ff5p',
         event.target,
         'Kr_81zp2OH0bmi1KC'
     ).then(() => {
-        console.log('this worked1')
-    }, 500);
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "the email service is temporarily unavailable. please contact me at email@email.com"
+        )
+    }; 
+     }) 
 }
+
+
